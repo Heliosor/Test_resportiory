a5ad0a65165cbefd12962253f2e75c2c7cd93415
李芳 <2658085067@qq.com>
2023-04-10 22:14:10 +0800

    回滚回 001

--- src/Week.java
+++ src/Week.java
@@ -2,6 +2,5 @@ public class Week {
     public static void main(String[] args) {
         System.out.println("第一次工作  回滚点1");
         System.out.println("我要回滚  001");
-        System.out.println("我要回滚  002");
     }
 }
